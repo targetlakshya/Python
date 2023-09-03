ca4a12c71725e0460a64261695ba415e356d20bf
targetlakshya <thetarget018@gmail.com>
2023-09-03 21:06:58 +0530

    Basics of python

--- intro.py
+++ intro.py
@@ -1,7 +1,63 @@
 # variables
 
+# Integer Variable
 laks = 10
 print(laks)
 
 vik = 19
 print(laks + vik )
+
+print(type(laks))
+
+# Float Variable
+b = 2.09 
+print(type(b))
+
+# String Variable
+d = "Lakshya"
+print(type(d))
+
+e = "This is my tech  class"
+print(type(e))
+
+# Boolean Variable
+n = True
+print(type(n))
+
+m = False 
+print(type(m))
+
+print(n + m)
+print(n*m) 
+# print(n/m)  # --> Division By zero error
+
+# Complex Variable
+v = 5 + 8j # here j is the notation which can coclude it is imaginary
+print(type(v))
+print(v.real) # throw real value of the complex 
+print(v.imag) #throw imaginary value of the complex
+
+# How to Print
+print("My name is Lakshya")
+print(v)
+
+s = "PWskills"
+print(s[0]) # Indexing (Index always starting from 0 [Forward])
+
+print(s[-1]) # Backward indexing
+print(s[-7])
+
+print(s[0:3]) # Slicing [Starting : End - 1]
+
+print(s[0:8:2]) # [Start : end -1 : jump]
+print(s[::2])
+
+
+
+ 
+
+
+
+
+
+
