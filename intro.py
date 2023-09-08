4c7b65d67acd281751665f690182f1752a389091
targetlakshya <thetarget018@gmail.com>
2023-09-08 14:20:28 -0700

    Python Basics

--- intro.py
+++ intro.py
@@ -92,13 +92,84 @@ l.append(5)
 l.append(s)
 l.append(l1)
 l[-1][1]
-
-# l.extend("laksjya")
-l.extend(l1)
 print(l)
 
-l1.insert(1,"divyansh")
+l.extend("lakshya") # only for iterable object such as string
+# l.append("Lakshhya")
+l.extend([7,8,9,10])
+print(l)
+
+l1.insert(1,"Lakshya")
+l1.insert(3,"saurabh")
 print(l1)
+
+# l1.pop() # last element deleted  
+l1.pop(0) # works on index
+
+l1.remove("Lakshya")
+l1.append([3,4,5])
+l1[3].remove(3)
+print(l1)
+
+l1.reverse()
+print(l1)
+
+l2 = [3,4,5,6,3,5,67,2,0]
+l2.sort()
+print(l2)
+
+l3 = ["lakshya", "saurabh", "pawan"]
+l3.sort()
+print(l3)
+
+l2.sort(reverse=True) #reverse
+print(l2)
+
+print(l3.index("lakshya"))
+
+l4 = [3,4,5,6]
+l4[0] = 30
+print(l4)
+
+s = "laakshya"
+s.replace("l", 'a')
+print(s) # --> laakshya nvz str is immutable
+
+# Tuples
+
+t = (2,3,4,5,"lakshya",45.56,False, 45+456j, [2,3,4])
+print(t)
+
+# print(type(t))
+print(len(t))
+print(t[0])
+print(t[::-1])
+print(t.count(5))
+print(t.index(False))
+
+# Sets
+
+# s2 = {2,3,4,5}
+# print(type(s2))
+s2 = {2,3,4,5,"lakshya",45.56,False, 45+456j, (2,3,4)}
+print(s2)
+
+s3 = {2,3,4,2,4,5,6,6,5,4,3,3,2,4,5,6,5,4,2,1,4,6,7,8,9,6,5,4,}
+print(s3)
+
+# print(s3[::-1])
+
+s4 = {12,2,23,234,342,4,45,456,567,789,'abc'}
+s4.remove(2)
+print(s4)
+
+
+
+
+
+
+
+
 
 
 
